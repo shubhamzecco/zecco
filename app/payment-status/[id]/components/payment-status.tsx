0545c5fe29b60a93b94fc178a725b00bf83f1ad9
Mrunal <mrunal@appristine.in>
2026-05-19 10:16:24 +0530

    Changes

--- app/payment-status/[id]/components/payment-status.tsx
+++ app/payment-status/[id]/components/payment-status.tsx
@@ -237,7 +237,7 @@ export function PaymentStatus({
                   const payload = {
                     package_id: package_id,
                     user_id: user_data?.user?._id,
-                    webhook_url: `https://living-sin-headlines-lucky.trycloudflare.com `,
+                    // webhook_url: `https://living-sin-headlines-lucky.trycloudflare.com `,
                   };
                   CommonApiRequest(
                     "POST",
