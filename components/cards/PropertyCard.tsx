f9a2c0ab22ed4584f143d31039b42a658168f290
shubhamzecco <shubham.zecco@gmail.com>
2026-07-24 15:18:42 +0530

    feat : Favorite properties horizontal scroll add

--- components/cards/PropertyCard.tsx
+++ components/cards/PropertyCard.tsx
@@ -360,13 +360,9 @@ const PropertyCard = ({
             ? property?.propertyType?.name
             : property?.propertyCategory?.name}{" "}
           for{" "}
-          {property?.isSale && property?.isRent
-            ? "Sale or Rent"
-            : property?.isSale
-              ? "Sale"
-              : property?.isRent
-                ? "Rent"
-                : ""}{" "}
+          {property?.isSale
+            ? "Sale"
+            : ""}{" "}
           in {property?.locationSubarea ? `${property?.locationSubarea},` : ""}{" "}
           {property?.locationArea ? `${property?.locationArea},` : ""}{" "}
           {property?.locationCity}, {property?.locationCountry}
@@ -381,7 +377,7 @@ const PropertyCard = ({
           ) : property?.mtsPlot !== null && property?.mtsPlot !== undefined ? (
             <div className="flex font-manrope font-normal items-center gap-1">
               <p className="text-xs font-manrope font-semibold text-[#6B7280] uppercase">
-                Plot area : 
+                Plot area :
               </p>
               <span>{property?.mtsPlot} m²</span>
             </div>
