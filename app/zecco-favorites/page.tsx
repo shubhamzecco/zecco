c694e69bfca5c8d3f19096da97975e52d105172e
shubhamzecco <shubham.zecco@gmail.com>
2026-09-09 18:12:55 +0530

    export const revalidate = 300; add in the fevorite page

--- app/zecco-favorites/page.tsx
+++ app/zecco-favorites/page.tsx
@@ -5,7 +5,7 @@ import {
   serverFetchLocationList,
 } from "@/lib/serverActions";
 
-export const dynamic = "force-dynamic";
+export const revalidate = 300;
 
 export const metadata: Metadata = {
   title: "Zecco's Handpicked Favorites | Premium Spain Properties",
